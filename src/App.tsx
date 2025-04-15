096a9d1f3f4c007af92155035a8f4e24558be4b6
ashmuradyann <69338957+ashmuradyann@users.noreply.github.com>
2025-04-15 21:26:04 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,7 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import Main from "./components/main/Main";
 
 const App: React.FC = (): any => {
-  return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-    </Routes>
-  );
+  return <Main />;
 };
 
 export default App;
